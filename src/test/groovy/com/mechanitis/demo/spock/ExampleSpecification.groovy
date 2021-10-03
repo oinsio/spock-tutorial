948b14dedfd9426667f71ea1d890948de32c3364
oinsio <oinsio.srv@gmail.com>
2021-10-03 11:56:01 +0300

    setup and teardown examples

--- src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
+++ src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
@@ -5,6 +5,22 @@ import spock.lang.Subject
 
 class ExampleSpecification extends Specification {
 
+    void setupSpec() {
+        // setup code that needs to be run once before all methods in this class
+    }
+
+    void setup() {
+        // setup code that needs to be run before every test method
+    }
+
+    void cleanup() {
+        // code that tears down things at the end of every test method
+    }
+
+    void cleanupSpec() {
+        // code that tears down everything at the end of all tests have run
+    }
+
     def "should be a simple assertion"() {
         expect:
             1 == 1
