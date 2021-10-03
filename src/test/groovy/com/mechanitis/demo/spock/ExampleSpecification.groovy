17af517fa609b122b674dfcc1a5ec5e3ede2542d
oinsio <oinsio.srv@gmail.com>
2021-10-03 11:08:16 +0300

    fix test

--- src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
+++ src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
@@ -128,8 +128,8 @@ class ExampleSpecification extends Specification {
 
         then:
             verifyAll(polygon) {
-                numberOfSides == 5
-                renderer == null
+                numberOfSides == 4
+                renderer == renderer
             }
     }
 }
