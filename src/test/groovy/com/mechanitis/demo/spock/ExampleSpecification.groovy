ff1ca147a8a9c01e21b14e77555a60651326620c
oinsio <oinsio.srv@gmail.com>
2021-10-03 07:03:08 +0300

    add first test

---
+++ src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
@@ -0,0 +1,11 @@
+package com.mechanitis.demo.spock
+
+import spock.lang.Specification
+
+class ExampleSpecification extends Specification {
+
+    def "should be a simple assertion"() {
+        expect:
+            1 == 1
+    }
+}
