d7b72f3bc01c3d5a34c0275402d1a5ff07053c2c
oinsio <oinsio.srv@gmail.com>
2021-10-03 09:03:17 +0300

    should be able to create a polygon with #sides sides

--- src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
+++ src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
@@ -41,4 +41,12 @@ class ExampleSpecification extends Specification {
         where:
             sides << [-1, 0, 1, 2]
     }
+
+    def "should be able to create a polygon with #sides sides"() {
+        expect:
+            new Polygon(sides).numberOfSides == sides
+
+        where:
+            sides << [3, 4, 5, 6, 7, 8, 14]
+    }
 }
