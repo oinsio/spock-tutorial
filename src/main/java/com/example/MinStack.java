af8e1daef7b8b653f563ae7ed479dca54e6b81ce
oinsio <8233537+oinsio@users.noreply.github.com>
2025-10-31 18:36:47 +0500

    LinkedList-base solution

---
+++ src/main/java/com/example/MinStack.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+class MinStack {
+
+    /*
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+    Implement the MinStack class:
+
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+    You must implement a solution with O(1) time complexity for each function.
+
+    -231 <= val <= 231 - 1
+    Methods pop, top and getMin operations will always be called on non-empty stacks.
+    At most 3 * 104 calls will be made to push, pop, top, and getMin.
+     */
+
+    private final List<Integer> stack;
+    private final List<Integer> minStack;
+
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        stack.addFirst(val);
+        if (minStack.isEmpty() || minStack.getFirst() >= val) minStack.addFirst(val);
+    }
+
+    public void pop() {
+        if (minStack.getFirst().equals(stack.removeFirst())) minStack.removeFirst();
+    }
+
+    public int top() {
+        return stack.getFirst();
+    }
+
+    public int getMin() {
+        return minStack.getFirst();
+    }
+}
+
+
---
+++ src/test/groovy/com/example/MinStackTest.groovy
@@ -0,0 +1,115 @@
+package com.example
+
+import spock.lang.Specification
+
+
+class MinStackTest extends Specification {
+
+    /*
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+    Implement the MinStack class:
+
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+    You must implement a solution with O(1) time complexity for each function.
+     */
+
+    def "should return pushed ints in the reverse order"() {
+        given:
+        def stack = new MinStack()
+        stack.push(10)
+        stack.push(20)
+        stack.push(30)
+
+        expect:
+        stack.top() == 30
+        stack.pop()
+        and:
+        stack.top() == 20
+        stack.pop()
+        and:
+        stack.top() == 10
+    }
+
+    def "should return min value from the stack"() {
+        given:
+        def stack = new MinStack()
+        stack.push(1)
+        stack.push(5)
+
+        expect:
+        stack.getMin() == 1
+    }
+
+    def "should return the previous min value from the stack after pop the top min value"() {
+        given:
+        def stack = new MinStack()
+        stack.push(10)
+        stack.push(50)
+        stack.push(5)
+
+        expect:
+        stack.top() == 5
+        stack.getMin() == 5
+        and:
+        stack.pop()
+        stack.top() == 50
+        stack.getMin() == 10
+        and:
+        stack.pop()
+        stack.top() == 10
+        stack.getMin() == 10
+    }
+
+    def "should work with a lot of commands"() {
+        given: "MinStack, push, push, push"
+        def stack = new MinStack()
+        stack.push(0)
+        stack.push(1)
+        stack.push(0)
+
+        expect: "getMin, pop, getMin, pop, getMin, pop"
+        stack.getMin() == 0
+        stack.pop()
+        stack.getMin() == 0
+        stack.pop()
+        stack.getMin() == 0
+        stack.pop()
+
+        and: "push, push, push"
+        stack.push(-2)
+        stack.push(-1)
+        stack.push(-2)
+
+        and: "getMin, pop, top, getMin, pop, getMin, pop"
+        stack.getMin() == -2
+        stack.pop()
+        stack.top() == -1
+        stack.getMin() == -2
+        stack.pop()
+        stack.getMin() == -2
+        stack.pop()
+    }
+
+    def "should work with another list of commands"() {
+        given:
+        def stack = new MinStack()
+        stack.push(2)
+        stack.push(0)
+        stack.push(3)
+        stack.push(0)
+
+        expect:
+        stack.getMin() == 0
+        stack.pop()
+        stack.getMin() == 0
+        stack.pop()
+        stack.getMin() == 0
+        stack.pop()
+        stack.getMin() == 2
+    }
+}
