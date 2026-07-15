d96b65e8ba4cf4bf3854100d35863a5e583699a9
oinsio <8233537+oinsio@users.noreply.github.com>
2026-07-15 20:58:46 +0500

    upgrade java and lib versions

--- src/test/groovy/com/mechanitis/demo/spock/DynamoDbLocalstackTest.groovy
+++ src/test/groovy/com/mechanitis/demo/spock/DynamoDbLocalstackTest.groovy
@@ -1,30 +1,29 @@
 package com.mechanitis.demo.spock
 
 import spock.lang.Shared
-import com.amazonaws.auth.AWSStaticCredentialsProvider
-import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.services.dynamodbv2.model.AttributeValue
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest
-import com.amazonaws.services.dynamodbv2.document.DynamoDB
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
-import static com.amazonaws.services.dynamodbv2.model.KeyType.HASH
 import org.testcontainers.containers.localstack.LocalStackContainer
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.DYNAMODB
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement
+import software.amazon.awssdk.services.dynamodb.model.KeyType
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType
 
 class DynamoDbLocalstackTest extends LocalstackSpecification {
 
     @Shared
     LocalStackContainer localstack = new LocalStackContainer(localstackImage).withServices(DYNAMODB)
     @Shared
-    AmazonDynamoDB dynamoDbClient
-    @Shared
-    DynamoDB dynamoDB
+    DynamoDbClient dynamoDbClient
 
     @Shared
     def tableName = "Users"
@@ -33,68 +32,60 @@ class DynamoDbLocalstackTest extends LocalstackSpecification {
 
     def setupSpec() {
 
-        def endpoint = new AwsClientBuilder.EndpointConfiguration(
-                localstack.getEndpointOverride(DYNAMODB).toString(),
-                localstack.getRegion()
-        )
-
-        dynamoDbClient = AmazonDynamoDBClientBuilder
-            .standard()
-            .withEndpointConfiguration(endpoint)
-            .withCredentials(new AWSStaticCredentialsProvider(
-                    new BasicAWSCredentials(localstack.getAccessKey(), localstack.getSecretKey())
+        dynamoDbClient = DynamoDbClient.builder()
+            .endpointOverride(localstack.getEndpointOverride(DYNAMODB))
+            .credentialsProvider(StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(localstack.getAccessKey(), localstack.getSecretKey())
             ))
+            .region(Region.of(localstack.getRegion()))
             .build()
 
-        dynamoDB = new DynamoDB(dynamoDbClient)
-
         // Create Table Users
-        def attributeDefinitions = new ArrayList<AttributeDefinition>()
-        attributeDefinitions.add(new AttributeDefinition()
-            .withAttributeName("Id")
-            .withAttributeType("N"))
+        dynamoDbClient.createTable(CreateTableRequest.builder()
+            .tableName(tableName)
+            .attributeDefinitions(AttributeDefinition.builder()
+                .attributeName("Id")
+                .attributeType(ScalarAttributeType.N)
+                .build())
+            .keySchema(KeySchemaElement.builder()
+                .attributeName("Id")
+                .keyType(KeyType.HASH)
+                .build())
+            .provisionedThroughput(ProvisionedThroughput.builder()
+                .readCapacityUnits(5L)
+                .writeCapacityUnits(6L)
+                .build())
+            .build())
 
-        def keySchema = new ArrayList<KeySchemaElement>()
-        keySchema.add(new KeySchemaElement()
-            .withAttributeName("Id")
-            .withKeyType(HASH))
-
-        def request = new CreateTableRequest()
-            .withTableName(tableName)
-            .withKeySchema(keySchema)
-            .withAttributeDefinitions(attributeDefinitions)
-            .withProvisionedThroughput(new ProvisionedThroughput()
-                .withReadCapacityUnits(5L)
-                .withWriteCapacityUnits(6L))
-
-        def table = dynamoDB.createTable(request)
-        table.waitForActive()
+        dynamoDbClient.waiter().waitUntilTableExists { it.tableName(tableName) }
 
         // Insert Item into Table Users
-        def attributeValues = new HashMap<String, AttributeValue>()
-        attributeValues.put("Id", new AttributeValue().withN(id.toString()))
-        attributeValues.put("Email", new AttributeValue().withS("foo@bar.com"))
-        attributeValues.put("FullName", new AttributeValue().withS("Foo Bar"))
-
-        PutItemRequest putItemRequest = new PutItemRequest()
-            .withTableName(tableName)
-            .withItem(attributeValues)
-        dynamoDbClient.putItem(putItemRequest)
+        dynamoDbClient.putItem(PutItemRequest.builder()
+            .tableName(tableName)
+            .item([
+                "Id"      : AttributeValue.builder().n(id.toString()).build(),
+                "Email"   : AttributeValue.builder().s("foo@bar.com").build(),
+                "FullName": AttributeValue.builder().s("Foo Bar").build()
+            ])
+            .build())
     }
 
     def "should access table Users"() {
 
         expect:
-            dynamoDB.getTable(tableName).describe().getTableName() == "Users"
+            dynamoDbClient.describeTable { it.tableName(tableName) }.table().tableName() == "Users"
     }
 
     def "should read inserted User from DynamoDB"() {
 
         when:
-            def user = dynamoDB.getTable(tableName).getItem("Id", id)
+            def user = dynamoDbClient.getItem(GetItemRequest.builder()
+                .tableName(tableName)
+                .key(["Id": AttributeValue.builder().n(id.toString()).build()])
+                .build()).item()
         then:
-            user.get("Id") == 1
-            user.get("Email") == "foo@bar.com"
-            user.get("FullName") == "Foo Bar"
+            user.get("Id").n() == "1"
+            user.get("Email").s() == "foo@bar.com"
+            user.get("FullName").s() == "Foo Bar"
     }
 }
